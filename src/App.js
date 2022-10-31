ce3a8f3ce229eef2beeed11b0474b5e061da0813
Krishnendu <krishnendu.kalyani1@gmail.com>
2022-10-31 12:02:51 +0530

    Linkedin upto rightside

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Login from './components/Login';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </Router>
+    </div >
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const Main = (props) => {
+    return (
+        <Cointainer>Main</Cointainer>
+    )
+}
+
+const Cointainer = styled.div`
+    grid-area: main;
+`;
+export default Main
